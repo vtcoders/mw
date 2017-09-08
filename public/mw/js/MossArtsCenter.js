bf79052be37fac1bdfe263fec2df1a87995aca95
Karsten <kars10@vt.edu>
2017-09-08 14:11:01 -0400

    remove unnecessary files and configure make files to download bootstrap.css

--- public/mw/js/MossArtsCenter.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-    var prefix = mw_getScriptOptions().prefix;
-
-    //Add MAC Model to the Scene
-    mw_addActor(prefix + '');
-
-    mw_addActor(prefix + '../subscriptions/viewpointAvatar.js');
-})
